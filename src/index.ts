e19351170c769fecaf7c664e0019e32a3d84b645
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-04-07 23:28:23 -0300

    feat: added cors rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { Server } from "./server/Server";
+import { Knex } from "./server/database/knex";
 
-Server.listen(process.env.PORT || 3001,()=>{
-    console.log(`rodando na porta ${process.env.PORT || 3001}`);
-})
+const startServer = () => {
+    Server.listen(process.env.PORT || 3333, () => {
+        console.log(`rodando na porta ${process.env.PORT || 3333}`);
+    });
+}
+
+if(!process.env.IS_LOCALHOST){
+    Knex.migrate.latest().then(()=>{
+        startServer();
+    }).catch(console.log);
+}else{
+    startServer();
+}
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 import 'dotenv/config';
 
 const Server = express();
+
+Server.use(cors({
+    origin: 'https://www.google.com'
+}))
 
 Server.use(express.json());
 Server.use(router);
